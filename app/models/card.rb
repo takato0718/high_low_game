95042f1d7d3c44c9c04f3bd6c1ce298d09dde2e8
takato0718 <takato.takashi.0718@outlook.jp>
2025-08-14 14:05:36 +0900

    gamesモデルとviewの実装

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,13 @@
 class Card
     SUITS = %w[♠ ♥ ♦ ♣].freeze
     RANKS = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
+
+    SUIT_SYMBOLS = {
+    'spades'   => '♠',
+    'hearts'   => '♥', 
+    'diamonds' => '♦',
+    'clubs'    => '♣'
+    }.freeze
     
     attr_reader :suit, :rank
     
@@ -26,6 +33,51 @@ class Card
     def color
       %w[♥ ♦].include?(@suit) ? 'red' : 'black'  #♥ ♦は赤でそれ以外は黒
     end
+
+    def suit_symbol
+      @suit
+    end
+      
+    def display_number
+      @rank
+    end
+
+    def css_classes
+      "playing-card #{color}"
+    end    
+
+    def suit_name
+      case @suit
+      when '♠' then 'spades'
+      when '♥' then 'hearts'
+      when '♦' then 'diamonds'
+      when '♣' then 'clubs'
+      end
+    end
+
+    def sprite_class
+      card_value = case value
+                    when 1 then 'A'
+                    when 11 then 'J'
+                    when 12 then 'Q'
+                    when 13 then 'K'
+                    else value.to_s
+                    end
+        
+      "card-#{suit_name}-#{card_value}"
+    end
+
+    def image_path
+      rank_name = case @rank
+      when 'A' then 'ace'
+      when 'J' then 'jack'
+      when 'Q' then 'queen'
+      when 'K' then 'king'
+      else @rank.downcase
+      end
+        
+        "cards/#{rank_name}_of_#{suit_name}.png"
+    end    
     
     # デッキ作成（クラスメソッド）
     def self.create_deck
