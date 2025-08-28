8648d90777861e1af2c0333a2c822c24df9dd4bf
takato0718 <takato.takashi.0718@outlook.jp>
2025-08-28 23:03:51 +0900

    Lintチェック追加+自動修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  #スタート画面をルートにする
+  # スタート画面をルートにする
   root 'games#index'
 
   # High&Lowゲーム用のルート
-  resources :games, only: [:index, :show] do
+  resources :games, only: %i[index show] do
     member do
-      post :guess  # 予想を送信するアクション
+      post :guess # 予想を送信するアクション
       post :reset_game_action, as: :reset_game # ゲームリセット用（後で追加）
     end
   end
@@ -14,11 +14,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
+  get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
